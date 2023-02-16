1dca7d79ea2e179cb5315fb885c988e5a8104355
ziad <ziadabdelaziz1995@gmail.com>
2023-02-16 02:58:22 +0200

    layout

--- lib/main.dart
+++ lib/main.dart
@@ -7,35 +7,105 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        scaffoldBackgroundColor: const Color(0xFF090e1c),
+        //container: 1b1a2e
+        //button: e9134c
+        //circles: 434554
+        primaryColor: const Color(0xFF1b1a2e),
+        colorScheme: ColorScheme.fromSwatch()
+            .copyWith(secondary: const Color(0xFFe9134c)),
+
+        textTheme: const TextTheme(
+          bodyLarge: TextStyle(
+              color: Colors.white, fontSize: 26, fontWeight: FontWeight.bold),
+          bodyMedium: TextStyle(
+              color: Colors.white, fontSize: 20, fontWeight: FontWeight.bold),
+          labelMedium: TextStyle(color: Color(0xFF434554), fontSize: 18),
+        ),
       ),
-      home: const MyHomePage(),
+      home: const Calculator(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key});
+class Calculator extends StatefulWidget {
+  const Calculator({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<Calculator> createState() => _CalculatorState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
+class _CalculatorState extends State<Calculator> {
   @override
   Widget build(BuildContext context) {
+    final theme = Theme.of(context);
     return Scaffold(
       appBar: AppBar(
-        title: Text("yub"),
+        title: const Text('BMI CALCULATOR'),
+        centerTitle: true,
+        backgroundColor: Theme.of(context).scaffoldBackgroundColor,
       ),
-      body: Text('hello there'));
+      body: Column(
+        children: [
+          Expanded(
+            child: Row(
+              children: const [
+                Cell(child: Center(child: Text('yub'))),
+                Cell(child: Center(child: Text('yub'))),
+              ],
+            ),
+          ),
+          const Cell(child: Center(child: Text('yub'))),
+          Expanded(
+            child: Row(
+              children: const [
+                Cell(child: Center(child: Text('yub'))),
+                Cell(child: Center(child: Text('yub'))),
+              ],
+            ),
+          ),
+          MaterialButton(
+            onPressed: () {},
+            color: theme.colorScheme.secondary,
+            minWidth: double.infinity,
+            padding: const EdgeInsets.all(25),
+            child: Text(
+              'CALCULATE',
+              style: theme.textTheme.bodyMedium,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class Cell extends StatelessWidget {
+  const Cell({
+    super.key,
+    required this.child,
+  });
+
+  final Widget child;
+  @override
+  Widget build(BuildContext context) {
+  final theme = Theme.of(context);
+    return Expanded(
+      child: Padding(
+        padding: const EdgeInsets.all(15),
+        child: ClipRRect(
+          borderRadius: const BorderRadius.all(Radius.circular(8)),
+          child: Container(
+            color: theme.primaryColor,
+            child: child,
+          ),
+        ),
+      ),
+    );
   }
 }
